28fb41ed98521e5fd2293ca7cef0ed216633afe3
TrumanH <TrumanH@users.noreply.github.com>
2017-12-06 15:44:59 +0800

    TrumanH
    img

--- hexun/hexun/spiders/hexunspd.py
+++ hexun/hexun/spiders/hexunspd.py
@@ -19,8 +19,8 @@ class HexunspdSpider(scrapy.Spider):
         item = HexunItem()
         item['name']=response.xpath("//span[@class='ArticleTitleText']/a/text()").extract()
         item["url"]=response.xpath("//span[@class='ArticleTitleText']/a/@href").extract()
-        #使用urllib和re模块获取博文的评论数和阅读数
-        #首先构造提取存储评论数和点击数网址的正则表达式
+        #接下来需要使用urllib和re模块获取博文的评论数和阅读数
+        #首先提取存储评论数和点击数网址的正则表达式
         pat1='<script type="text/javascript" src="(http://click.tool.hexun.com/.*?)">'
         #hcurl为存储评论数和点击数的网址
         hcurl=re.compile(pat1).findall(str(response.body))[0]
@@ -31,18 +31,18 @@ class HexunspdSpider(scrapy.Spider):
         opener.addheaders = [headers2]
         # 将opener安装为全局
         urllib.request.install_opener(opener)
-        #data为对应博客列表页的所有博文的点击数与评论数
+        #data为对应博客列表页的所有博文的点击数与评论数数据
         data=urllib.request.urlopen(hcurl).read()
-        #阅读数的正则式
+        #pat2为提取文章阅读数的正则表达式
         pat2="click\d*?','(\d*?)'"
-        #评论数的正则式
+        #pat3为提取文章评论数的正则表达式
         pat3="comment\d*?','(\d*?)'"
         #提取阅读数和评论数数据并分别赋值给item下的hits和comment
         item["hits"]=re.compile(pat2).findall(str(data))
         #print(item["hits"])
         item["comment"]=re.compile(pat3).findall(str(data))
         yield item
-        #博文列表页的总页数的正则式
+        #提取博文列表页的总页数
         pat4="blog.hexun.com/p(.*?)/"
         #通过正则表达式获取到的数据为一个列表，倒数第二个元素为总页数
         data2=re.compile(pat4).findall(str(response.body))
@@ -50,7 +50,8 @@ class HexunspdSpider(scrapy.Spider):
             totalurl=data2[-2]
         else:
             totalurl=1
-        #print("一共"+str(totalurl)+"页") #调试用
+        #在实际运行中，下一行print的代码可以注释掉，在调试过程中，可以开启下一行print的代码
+        #print("一共"+str(totalurl)+"页")
         #进入for循环，依次爬取各博文列表页的博文数据
         for i in range(2,int(totalurl)+1):
             #构造下一次要爬取的url，爬取一下页博文列表页中的数据
